b2bc486f60460bc50e6f3111ed5e84f11e5c6833
xboxone08 <sondad2008@gmail.com>
2020-12-30 13:14:10 -0500

    Update chat.py
    Fixed bugs

--- chat.py
+++ chat.py
@@ -5,4 +5,4 @@ game = minecraft.Minecraft.create()
 while True:
     command = input()
     if command[0] != "/":
-        game.postToChat("<StevePi_0> [ADMIN]" + command)
+        game.postToChat("<StevePi_0> [ADMIN] " + command)
