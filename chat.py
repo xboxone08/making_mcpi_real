1d80b3e6b75ad01e350da95e556c54437eb2f8bf
xboxone08 <sondad2008@gmail.com>
2020-12-30 12:24:35 -0500

    Update chat.py

--- chat.py
+++ chat.py
@@ -3,4 +3,4 @@ import making_mcpi_real as main
 while True:
     command = input()
     if command[0] != "/":
-        main.game.postToChat(command)
+        main.game.postToChat("<StevePi_0> [ADMIN]" + command)
