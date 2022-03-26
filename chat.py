0314b417843dbe09a3a2b09e852e7b613f8643b3
xboxone08 <sondad2008@gmail.com>
2022-03-26 00:47:24 -0400

    mcpi files

--- chat.py
+++ chat.py
@@ -1,21 +1,9 @@
 import mcpi.minecraft as minecraft
-from subprocess import Popen
 from time import sleep
-import pygame
-
-pygame.init()
-
-clock = pygame.time.Clock()
 
 game = minecraft.Minecraft.create()
 
-sleep(20)
-
-iteration: int = 1
-
 while True:
-    clock.tick(1)
-
     command = input(">")
     if command[0] != "/":
         game.postToChat("<StevePi_0> [ADMIN] " + command)
@@ -25,3 +13,4 @@ while True:
                 if line[:line.index("=")] == "0":
                     game.postToChat("[@]" + line[line.index("=") + 1:])
                     break
+    sleep(0.2)
